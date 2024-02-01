120c93fd2f19370e35ab8228705a9e17cef28de2
Freynhel <97458860+Freynhel@users.noreply.github.com>
2024-02-01 04:47:33 -0300

    Added Stats, Business, Billing, Button & CardDeal

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Button = () => {
-	return (
-		<div>Button</div>
-	)
-}
+const Button = ({ content, styles }) => (
+	<button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+		{content}
+	</button>
+)
 
 export default Button
